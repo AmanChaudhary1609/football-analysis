f8a6c0a69b4d1f7abfc72f465dbd40a82206a1b4
Aman Chaudhary <amn234singh@gmail.com>
2025-07-02 17:39:52 +0530

    training

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,10 +1,15 @@
 from ultralytics import YOLO 
 
-model = YOLO('yolov8n')
+model = YOLO('yolo11x')
 # model.to('cuda')
 
-results = model.predict('input_videos/08fd33_4.mp4',save=True,device='0')
-print(results[0])
-print('=====================================')
-for box in results[0].boxes:
-    print(box)
+results = model.predict('input_videos/08fd33_4.mp4',save=True,device='0', stream=True)
+
+# print(results[0])
+# print('=====================================')
+# for box in results[0].boxes:
+#     print(box)
+
+for r in results:
+    print(r)  # or r.boxes, r.probs, etc.
+    # break  # remove this to process all frames
